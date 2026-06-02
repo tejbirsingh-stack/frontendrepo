a6cdffccec58303da559f54bcfa25cb67bf2a7d3
Tejbir Singh <tejbir-singh@cssoftsolutions.com>
2026-06-02 17:10:04 +0530

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -12,9 +12,9 @@ import './config/axios'; // Setup axios interceptors
 import './styles/noah-design-system.css';
 
 // Toast Notifications Component
-// dfgndjsgnjdsgnfdsfdsfdsf
+// dfgndjsgnjdsgnfdsfdsfdsf ddsdfsd
 function ToastNotifications() {
-  return null; // Placeholder for now
+  return null ; // Placeholder for now
 }
 
 // Upload component with drag & drop functionality
