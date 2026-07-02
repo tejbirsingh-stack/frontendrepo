cffd277497a59c5a3f26a381ac49c8f544118568
Tejbir Singh <tejbir-singh@cssoftsolutions.com>
2026-07-02 12:54:36 +0530

    hmrfalseremove

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig({
     allowedHosts: ['qa.noahcloud.ai', 'all'],
     port: 3002,
     host: true,
-    hmr: false,
     proxy: {
       '/api': {
         target: 'http://localhost:3000',
