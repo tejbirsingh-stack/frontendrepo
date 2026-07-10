01e33a70da8b3a6de8db75a57bfc0762b0f2da68
Soham Kashyap <soham-kashyap@cssoftsolutions.com>
2026-07-10 14:27:27 +0530

    microsoft-qa-fix-10July2026

--- src/config/msalConfig.ts
+++ src/config/msalConfig.ts
@@ -2,7 +2,7 @@ import type { Configuration } from "@azure/msal-browser";
 
 export const msalConfig: Configuration = {
     auth: {
-        clientId: import.meta.env.VITE_MICROSOFT_CLIENT_ID, // Your Client ID from Azure
+        clientId: import.meta.env.VITE_MICROSOFT_CLIENT_ID || "c3f3c839-56fa-414c-97c8-46a02c1a6147", // Fallback to bypass IT env injection issues
         authority: "https://login.microsoftonline.com/common", // "common" allows personal + business accounts
         redirectUri: "/",
         navigateToLoginRequestUrl: false,
