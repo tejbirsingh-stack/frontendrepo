f567285f85bb2d1cf99dc20a7ce9c937b207eaac
Tejbir Singh <tejbir-singh@cssoftsolutions.com>
2026-09-11 10:43:10 +0530

    add changes

--- src/config/msalConfig.ts
+++ src/config/msalConfig.ts
@@ -2,7 +2,7 @@ import type { Configuration } from "@azure/msal-browser";
 
 export const msalConfig: Configuration = {
     auth: {
-        clientId: import.meta.env.VITE_MICROSOFT_CLIENT_ID || "c3f3c839-56fa-414c-97c8-46a02c1a6147",
+        clientId: import.meta.env.VITE_MICROSOFT_CLIENT_ID || "c583b1ae-a24e-4bfa-8f59-0d587210e63e",
         authority: "https://login.microsoftonline.com/common", // "common" allows personal + business accounts
         redirectUri: typeof window !== 'undefined' ? window.location.origin : "/",
         postLogoutRedirectUri: typeof window !== 'undefined' ? window.location.origin : "/",
